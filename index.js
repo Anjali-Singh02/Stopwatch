85acde684f27f1d4a4eb5c53e34ce92d60d0099a
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-02-20 12:06:44 +0530

    updated UI features

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
-let hours = document.getElementById('hr');
-let minutes = document.getElementById('min');
-let seconds = document.getElementById('sec');
+const time = document.getElementById('time');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const falgsDiv = document.getElementById('flags');
 
-let hh = 0
-let mm = 0
-let ss = 0
-let id;
-
-let start = () => {
-    id = setInterval(startStopWatch, 1000)
-}
-let startStopWatch = () => {
-    if (ss == 59) {
-        mm++;
-        ss=0;
-        if(mm==59){
-            hh++;
-            mm=0;
+var hh = 0,
+    mm = 0,
+    ss = 0,
+    id;
+const start = () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    id = setInterval(startStopWatch, 1000);
+};
+const startStopWatch = () => {
+    ss = (ss + 1) % 60;
+    if (ss === 0) {
+        mm = (mm + 1) % 60;
+        if (mm === 0) {
+            hh = (hh + 1) % 24;
         }
     }
-    else {
-        ss++;
-    }
 
-    hours.innerHTML = (hh < 10) ? "0" + hh : hh;
-    minutes.innerHTML = (mm < 10) ? "0" + mm : mm;
-    seconds.innerHTML = (ss < 10) ? "0" + ss : ss;
-}
+    const tempTime = `${hh < 10 ? '0' + hh : hh} : ${mm < 10 ? '0' + mm : mm
+        } : ${ss < 10 ? '0' + ss : ss}`;
+    time.innerHTML = tempTime;
+};
 
-let stop = () => {
+const updateFlags = () => {
+    var data = localStorage.getItem('flags');
+    data = data ? JSON.parse(data) : [];
+    const val = data.map(
+        (elem) =>
+            '<li><span>' +
+            elem.index +
+            ') </span>&nbsp;<span>' +
+            elem.time +
+            '</span></li>',
+    );
+    falgsDiv.innerHTML = val;
+};
+
+const setFlag = () => {
+    const tempTime = `${hh < 10 ? '0' + hh : hh} : ${mm < 10 ? '0' + mm : mm
+        } : ${ss < 10 ? '0' + ss : ss}`;
+    var data = localStorage.getItem('flags');
+    data = data ? JSON.parse(data) : [];
+    data.push({ index: data.length + 1, time: tempTime });
+
+    localStorage.setItem('flags', JSON.stringify(data));
+    updateFlags();
+};
+
+const resetFlags = () => {
+    localStorage.removeItem('flags');
+    updateFlags();
+};
+
+const stop = () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
     clearInterval(id);
-}
-let reset = () => {
+};
+const reset = () => {
+    startBtn.disabled = false;
     clearInterval(id);
-    hh = 0
-    mm = 0
-    ss = 0
-    hours.innerHTML = (hh < 10) ? "0" + hh : hh;
-    minutes.innerHTML = (mm < 10) ? "0" + mm : mm;
-    seconds.innerHTML = (ss < 10) ? "0" + ss : ss;
-}
+    hh = mm = ss = 0;
+
+    const tempTime = '0' + hh + ' : ' + '0' + mm + ' : ' + '0' + ss;
+    time.innerHTML = tempTime;
+};
+
+updateFlags();
